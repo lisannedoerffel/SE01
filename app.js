44b9d091506d75b294b7dc9a5eda19604ed54b57
lisannedoerffel <91543057+lisannedoerffel@users.noreply.github.com>
2021-11-29 17:27:09 +0100

    Add files via upload
    This is the solution for exercise no 3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let inputPopulation = [
-    [0, 1, 0, 1],
-    [0, 1, 0, 1],
-    [0, 1, 0, 1],
-    [0, 1, 0, 1]
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0]
 ];
 
 function neighborCount(i, j) {
@@ -48,3 +48,37 @@ for (let k = 0; k < inputPopulation.length; k++){
         }  
 };
 
+//amountOfNeighbors is a value on the heatmap (heatmap[k][l]=amountOfNeighbors), so it equals the number of neighbors
+//inhabitant is a value on the inputPopulation, if inhabitant = 1 the cell is alive, 0 means it is dead
+
+
+
+function lifeAndDeath (inhabitant, amountOfNeighbors){
+    if (inhabitant === 1) {
+        if (!(amountOfNeighbors == 2) &&  !(amountOfNeighbors == 3)){
+            inhabitant = 0;
+        }
+    } 
+    if(inhabitant === 0) {
+        if (amountOfNeighbors==3){
+            inhabitant = 1;
+        }
+    }
+    return inhabitant;
+}
+// This is a test: 
+// lifeAndDeath(0, 2) I excpect it to yield 0
+
+// This was another test:
+// let newInhabitant = lifeAndDeath(inputPopulation[0][2], heatmap[0][2])
+
+newPopulation = [];
+
+for (let k = 0; k < inputPopulation.length; k++){
+    newPopulation[k] = [];
+    for (let l= 0; l < inputPopulation[k].length; l++){
+        newPopulation[k][l] = lifeAndDeath(inputPopulation[k][l], heatmap[k][l])
+        }  
+};
+
+console.log(newPopulation)
